75f47e5957c788def037e3cd39921fd2a29c023b
Simon Poole <simon.poole@students.plymouth.ac.uk>
2018-03-09 20:57:58 +0000

    Canvas background

--- assets/main.js
+++ assets/main.js
@@ -12,3 +12,31 @@ $('.navbar-burger').on('click', function(e){
     $('.navbar-burger').toggleClass('is-active');
     $('.navbar-menu').toggleClass('is-active');
 })
+var landing = document.getElementById('canvas');
+landing.style.width="100%";
+landing.style.height="100%";
+landing.width = landing.offsetWidth;
+landing.height = landing.offsetHeight;
+var pattern;
+var variance = 1;
+var toggle = 1;
+var seed = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 24);
+function setCanvas(){
+	if(variance >= 200) toggle = -1;
+	if(variance <= 0) {
+		toggle = 1;
+		seed = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 24);
+	}
+	variance += (0.25 * toggle);
+	pattern = Trianglify({
+		width: landing.width,
+		height: landing.height,
+		x_colors: 'PuRd',
+		seed: seed,
+		cell_size: 50,
+		variance: variance
+	}).canvas(landing);
+	document.getElementById('body').replaceChild(pattern, landing);
+}
+setInterval(setCanvas, 1)
+
